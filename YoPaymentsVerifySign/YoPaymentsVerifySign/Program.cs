8d382b64dc0b588a14384571350b79a01735f4a8
Joseph Tabajjwa <joseph.tabajjwa@gmail.com>
2020-09-19 16:36:34 +0300

    First commit, added the source

---
+++ YoPaymentsVerifySign/YoPaymentsVerifySign/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace YoPaymentsVerifySign
+{
+    class MainClass
+    {
+        //Set this to the right path of the ceritificate file.
+        private static string publicKeyFile = "Yo_Uganda_Public_Certificate.crt";
+
+        public static void Main(string[] args)
+        {
+
+            /*In your code, obtain the following data from the POST fields of the request*/
+            String date_time = "2019-06-21 10:09:21";
+            String network_ref = "1462967855";
+            String external_ref = "3-Joseph-Tabajjwa";
+            String msisdn = "256783086794";
+            String narrative = "Testing";
+            String amount = "2000";
+            String signature = "RTyGxIwp83Lb9Lo03yGOSyXKDjY3vmgvPjoOzFb79CWvUttvnnFh4Ln1/Ur71YucjXpkfTdhdz2GyLAWVtCxl3iqox3haZMIX/9JVcYh4tt5zipwUo0CLgRVehsyJlUs70ph7TJ1KU/qMcOz60HWLsJDPv95n4Dqdh3bTHg/f+XovxD5Qde7sGEeXWnAQBlq5Bb2dFtw9k6vyI+4BE5h++CKgCr/7wzvKM3hij4mTqIRW0Z+DtZK7cIgtmckr0w7F9eW+YCiymTRP4sdRqinEvDADW49/dDLq1gTnO83RxpSTmHw5NavvRGjszC3Fgub5t2gT52Kr9oNHZhgiBZIDg==";
+
+            YoSignatureVerifier yoVerifierObj = new YoSignatureVerifier(publicKeyFile);
+            yoVerifierObj.SetDateTime(date_time);
+            yoVerifierObj.SetNetworkRef(network_ref);
+            yoVerifierObj.SetExternalRef(external_ref);
+            yoVerifierObj.SetMsisdn(msisdn);
+            yoVerifierObj.SetNarrative(narrative);
+            yoVerifierObj.SetAmount(amount);
+            yoVerifierObj.SetSignature(signature);
+
+            if (yoVerifierObj.verify())
+            {
+                Console.WriteLine("Signature verification passed!");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+                Console.WriteLine(yoVerifierObj.GetError());
+            }
+        }
+
+    }
+
+}
---
+++ YoPaymentsVerifySign/YoPaymentsVerifySign/YoSignatureVerifier.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace YoPaymentsVerifySign
+{
+    class YoSignatureVerifier
+    {
+        private String dateTime;
+        private String networkRef;
+        private String externalRef;
+        private String msisdn;
+        private String narrative;
+        private String amount;
+        private String signature;
+
+        private String publicKeyFile;
+
+        private String errorMessage = "";
+
+        public YoSignatureVerifier(String publicKeyFile_)
+        {
+            this.publicKeyFile = publicKeyFile_;
+        }
+
+        /*
+        * Sets the base64 signature field        
+        * @Param String dateTime: This is the datetime
+        *        
+        */
+        public void SetDateTime(String dateTime_)
+        {
+            this.dateTime = dateTime_;
+        }
+        /*
+        *       
+        * @Param String signature: This is the network reference.
+        *        
+        */
+        public void SetNetworkRef(String networkRef_)
+        {
+            this.networkRef = networkRef_;
+        }
+        /*
+        * Sets the base64 signature field        
+        * @Param String signature: This is the external ref
+        *        
+        */
+        public void SetExternalRef(String externalRef_)
+        {
+            this.externalRef = externalRef_;
+        }
+        /*
+        *    
+        * @Param String signature: This is the msisdn field of the transaction
+        *        
+        */
+        public void SetMsisdn(String msisdn_)
+        {
+            this.msisdn = msisdn_;
+        }
+        /*
+        *       
+        * @Param String narrative: This is the narrative of the transaction.
+        *        
+        */
+        public void SetNarrative(String narrative_)
+        {
+            this.narrative = narrative_;
+        }
+        /*
+        * Sets the amount field      
+        * @Param String amount: This is the amount of the transaction.
+        *        
+        */
+        public void SetAmount(String amount_)
+        {
+            this.amount = amount_;
+        }
+
+        /*
+        * Sets the base64 signature field        
+        * @Param String signature: This is the base64 encoded signature.
+        *        
+        */
+        public void SetSignature(String signature_)
+        {
+            this.signature = signature_;
+        }
+
+
+        /*
+        * Returns: True if the signature verification passed or False if it failed
+        * or an error occurred.       
+        */
+        public bool verify()
+        {
+            if (dateTime == null)
+            {
+                this.errorMessage = "dateTime Field is null";
+                return false;
+            }
+            if (amount == null)
+            {
+                this.errorMessage = "amount Field is null";
+                return false;
+            }
+            if (narrative == null)
+            {
+                this.errorMessage = "narrative Field is null";
+                return false;
+            }
+            if (networkRef == null)
+            {
+                this.errorMessage = "networkRef Field is null";
+                return false;
+            }
+            if (externalRef == null)
+            {
+                this.errorMessage = "externalRef Field is null";
+                return false;
+            }
+            if (msisdn == null)
+            {
+                this.errorMessage = "msisdn Field is null";
+                return false;
+            }
+
+            String signedData = dateTime + amount + narrative + networkRef + externalRef + msisdn;
+            bool v = VerifyTheSignature(signature, signedData);
+            if (v)
+            {
+                return true;
+            }
+            else
+            {
+                this.errorMessage = "Signature verification failed.";
+                return false;
+            }
+        }
+
+        public String GetError()
+        {
+            return this.errorMessage;
+        }
+
+        /*
+        * @Param signature: This is the base64 string of data data to be verified.
+        * @Param data: This is the string data that was signed       
+        *        
+        * Returns Bool: True if signature passed or false if failed | an error.       
+        */
+
+        private bool VerifyTheSignature(String base64Signature, String signedData)
+        {
+            try
+            {
+                byte[] signature_ = Convert.FromBase64String(base64Signature);
+
+                byte[] data = Encoding.UTF8.GetBytes(signedData);
+
+                if (!File.Exists(publicKeyFile))
+                {
+                    this.errorMessage = "Verification failed: PublicKey Certificate: "
+                        + publicKeyFile + " does not exists";
+                    return false;
+                }
+
+                var x509 = new X509Certificate2(publicKeyFile);
+
+                if (!(x509.PublicKey.Key is RSACryptoServiceProvider rsa))
+                {
+                    this.errorMessage = "Verification failed: Failed to load x509.PublicKey";
+                    return false;
+                }
+
+                string sha1O_id = CryptoConfig.MapNameToOID("SHA1");
+
+                //use the certificate to verify data against the signature
+                bool sha1_valid = rsa.VerifyData(data, sha1O_id, signature_);
+
+                return sha1_valid;
+            }
+            catch (Exception e)
+            {
+                this.errorMessage = "Verification failed: " + e.Message;
+                return false;
+            }
+        }
+    }
+}
